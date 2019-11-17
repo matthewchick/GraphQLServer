567f57f7920accd6a05e7e623430b4b67c27ee3d
Matthew Chick <c.chick@unsw.edu.au>
2019-11-18 09:25:11 +1100

    Nested query

--- schema/schema.js
+++ schema/schema.js
@@ -3,12 +3,7 @@ const graphql = require('graphql');
 const _ = require('lodash');
 const axios = require('axios');
 
-const {
-    GraphQLObjectType,
-    GraphQLString,
-    GraphQLInt,
-    GraphQLSchema
-} = graphql;
+const { GraphQLObjectType,GraphQLString,GraphQLInt,GraphQLSchema} = graphql;
 /*
 const users = [
     { id: '23', firstName: 'Bill', age: 20},
@@ -16,15 +11,30 @@ const users = [
 ]
 */
 // https://graphql.org/graphql-js/type/
+const CompanyType = new GraphQLObjectType({
+    name: 'Company',
+    fields: {
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString }
+    }
+});
 const UserType = new GraphQLObjectType({
     name: 'User',
     fields: {
         id: { type: GraphQLString },
         firstName: { type: GraphQLString },
-        age: { type: GraphQLInt }
+        age: { type: GraphQLInt },
+        company: {
+            type: CompanyType,
+            resolve(parentValue, args) {
+                console.log(parentValue, args);
+                return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+                    .then(response => response.data);
+            }
+        }
     }
 });
-
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
@@ -39,7 +49,6 @@ const RootQuery = new GraphQLObjectType({
         }       
     }
 });
-
 module.exports = new GraphQLSchema({
     query: RootQuery
 });
